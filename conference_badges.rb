66294884b5fe75d8bffc29c274538468b896fc53
Natalie Barba <nbarba2010@gmail.com>
2021-09-20 15:51:05 -0700

    commit tree 0b06e7842420a2645f5dd744e61884619cb6aed5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(namesArray)
+    namesArray.map do |name|
+        badge_maker(name)
+    end
+end
+
